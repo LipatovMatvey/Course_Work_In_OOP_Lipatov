71a469965944f7920acdef5f1d22755b72508995
Matvey <lmv180106@mail.ru>
2026-04-12 11:29:32 +0300

    Создал поля для класса Patient

--- Course_work_in_OOP_Lipatov/Course_work_in_OOP_Lipatov/Patient.cs
+++ Course_work_in_OOP_Lipatov/Course_work_in_OOP_Lipatov/Patient.cs
@@ -4,7 +4,51 @@ using System.Text;
 
 namespace Course_work_in_OOP_Lipatov
 {
-    internal class Patient
+    public class Patient
     {
+        /// <summary>
+        /// ID пациента
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Полное имя пациента
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Возраст пациента
+        /// </summary>
+        public int Age { get; set; }
+
+        /// <summary>
+        /// Пол пациента
+        /// </summary>
+        public string Gender { get; set; }
+
+        /// <summary>
+        /// Диагноз пациента
+        /// </summary>
+        public string Disease { get; set; }
+
+        /// <summary>
+        /// Тяжесть заболевания
+        /// </summary>
+        public string Severity { get; set; }
+
+        /// <summary>
+        /// Продолжительность заболевания в днях
+        /// </summary>
+        public int Duration { get; set; }
+
+        /// <summary>
+        /// Отделение больницы, в котором проходит лечение пациент
+        /// </summary>
+        public string Department { get; set; }
+
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        public Patient() { }
     }
 }
